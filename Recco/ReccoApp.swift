cad7bc433aabb4467373ec128cbc0ce04761a831
christenx <99223047+christen03@users.noreply.github.com>
2024-08-03 17:45:02 -0700

    Initial Commit

---
+++ Recco/ReccoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReccoApp.swift
+//  Recco
+//
+//  Created by Christen Xie on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ReccoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
